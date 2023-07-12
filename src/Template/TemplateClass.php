ece9aebcd98937bd802a703d6942b4b6621557c6
Ryan Spaeth <rspaeth@spaethtech.com>
2023-07-12 13:19:53 -0500

    Template rework

---
+++ src/Template/TemplateClass.php
@@ -0,0 +1,9 @@
+<?php /** @noinspection PhpUnused */
+declare(strict_types=1);
+
+namespace SpaethTech\UCRM\SDK\Template;
+
+class TemplateClass
+{
+
+}
---
+++ tests/Template/TemplateClassTests.php
@@ -0,0 +1,11 @@
+<?php /** @noinspection PhpUnused */
+declare(strict_types=1);
+
+namespace SpaethTech\UCRM\SDK\Tests\Template;
+
+use PHPUnit\Framework\TestCase;
+
+class TemplateClassTests extends TestCase
+{
+
+}
